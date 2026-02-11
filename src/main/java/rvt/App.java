a6a300f22181d3d388a3ea20684c8fa0db9526ff
Ricards <a240706rl@rvt.lv>
2026-02-11 05:26:07 -0800

    To do list (2 parts)

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,7 +1,15 @@
 package rvt;
 
+import java.util.Scanner;
+
+
 public class App {
     public static void main(String[] args) {
-        // Your code goes here...
+        TodoList list = new TodoList();
+        try (Scanner scanner = new Scanner(System.in)) {
+            UserInterface ui = new UserInterface(list, scanner);
+            ui.start();
+        }
+        
     }
 }
